0b43d5f760089c434413d312c03fdf875b677806
Will Sowerbutts <will@sowerbutts.com>
2014-07-18 21:24:56 +0100

    Update for this week's idea of how we detect UNA

--- flash4.c
+++ flash4.c
@@ -374,12 +374,13 @@ bool check_file_size(cpm_fcb *imagefile, bool allow_partial)
 
 access_t access_auto_select(void)
 {
+    unsigned int **bios_signature = (unsigned int **)BIOS_SIGNATURE_ADDR;
     // Note that versions of RomWBW before approx 2014-08 place a
     // signature at CPM_SIGNATURE_ADDR but not BIOS_SIGNATURE_ADDR.
     // Therefore we cannot rely on the latter to confirm if RomWBW
     // HBIOS is present or not.
 
-    if(*((unsigned int*)BIOS_SIGNATURE_ADDR) == BIOS_SIGNATURE_UNA)
+    if(**bios_signature == BIOS_SIGNATURE_UNA)
         return ACCESS_UNABIOS;
 
     if(*((unsigned int*)CPM_SIGNATURE_ADDR) == CPM_SIGNATURE_ROMWBW)
@@ -400,7 +401,7 @@ void main(int argc, char *argv[])
     action_t action = ACTION_UNKNOWN;
     access_t access = ACCESS_AUTO;
 
-    printf("FLASH4 by Will Sowerbutts <will@sowerbutts.com> version 1.0.4\n\n");
+    printf("FLASH4 by Will Sowerbutts <will@sowerbutts.com> version 1.0.5\n\n");
 
     /* determine access mode */
     for(i=1; i<argc; i++){ /* check for manual mode override */
