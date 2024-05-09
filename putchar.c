3d9109b75799f1d59a9c6f4386c60e028398201c
Will Sowerbutts <will@sowerbutts.com>
2024-05-09 22:46:52 +0100

    putchar.c: improve comments, save a whole byte

--- putchar.c
+++ putchar.c
@@ -7,42 +7,46 @@ __asm
 ; putchar routine for CP/M -- sdcccall(1) version
     ; char to print is in L
     push hl
+
+    ; print char
     ld e,l
     ld c,#2
     call 5
-    pop hl
 
-    ; we even handle CRLF correctly
+    ; check for CR
+    pop hl
     ld a, l
     cp #10
-    ret nz
+    ret nz      ; return if not CR
+
+    ; after CR always print LF
     ld e, #13
     ld c, #2
-    call 5
-
-    ret
+    jp 5        ; leave our return address on the stack
 __endasm;
 #else
 __asm
 ; putchar routine for CP/M -- sdcccall(0) version
+    ; char to print is in (SP+2)
     ld hl,#2
     add hl,sp
-
     push hl
+
+    ; print char
     ld e,(hl)
     ld c,#2
     call 5
-    pop hl
 
-    ; we even handle CRLF correctly
+    ; check for CR
+    pop hl
     ld a, (hl)
     cp #10
-    ret nz
+    ret nz      ; return if not CR
+
+    ; after CR always print LF
     ld e, #13
     ld c, #2
-    call 5
-
-    ret
+    jp 5        ; leave our return address on the stack
 __endasm;
 #endif
     /* this code is compiled but never used.
